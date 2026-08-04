5390806e64a6e8e2981573c18547fdf0d598991c
Rostyslav <rooostik2016@gmail.com>
2026-08-04 04:11:03 +0300

    Solution

---
+++ src/services/expense.service.js
@@ -0,0 +1,80 @@
+const userService = require('./user.service');
+const expenses = [];
+
+const getAll = ({ userId, from, to, categories }) => {
+  let result = [...expenses];
+
+  if (userId) {
+    result = result.filter((expense) => expense.userId === userId);
+  }
+
+  if (categories) {
+    result = result.filter((expense) => categories.includes(expense.category));
+  }
+
+  if (from) {
+    result = result.filter((item) => new Date(item.spentAt) >= new Date(from));
+  }
+
+  if (to) {
+    result = result.filter((item) => new Date(item.spentAt) <= new Date(to));
+  }
+
+  return result;
+};
+
+const getById = (id) => {
+  const expense = expenses.find((element) => element.id === id);
+
+  if (!expense) {
+    throw new Error('Not found');
+  }
+
+  return expense;
+};
+
+const create = (data) => {
+  if (!userService.getById(data.userId)) {
+    throw new Error('User not found');
+  }
+
+  const id = Math.ceil(Math.random() * 999999999999);
+  const expense = { id, ...data };
+
+  expenses.push(expense);
+
+  return expense;
+};
+
+const update = (id, data) => {
+  const expense = getById(id);
+
+  Object.keys(data).forEach((key) => {
+    if (data[key] === undefined) {
+      delete data[key];
+    }
+  });
+
+  Object.assign(expense, data);
+
+  return expense;
+};
+
+const remove = (id) => {
+  getById(id);
+
+  const index = expenses.findIndex((element) => element.id === id);
+
+  if (index !== -1) {
+    expenses.splice(index, 1);
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+  expenses,
+};
---
+++ src/services/user.service.js
@@ -0,0 +1,51 @@
+const users = [];
+
+const getAll = () => {
+  return [...users];
+};
+
+const getById = (id) => {
+  const user = users.find((element) => element.id === id);
+
+  if (!user) {
+    throw new Error('Not found');
+  }
+
+  return user;
+};
+
+const create = (name) => {
+  const id = Math.ceil(Math.random() * 999999999999);
+  const user = { id, name };
+
+  users.push(user);
+
+  return user;
+};
+
+const update = (id, data) => {
+  const user = getById(id);
+
+  Object.assign(user, data);
+
+  return user;
+};
+
+const remove = (id) => {
+  getById(id);
+
+  const index = users.findIndex((element) => element.id === id);
+
+  if (index !== -1) {
+    users.splice(index, 1);
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+  users,
+};
